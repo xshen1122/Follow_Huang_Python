be38763874b9416109ce977253e43a7908cbb552
xshen1122 <xueqin.shen@outlook.com>
2017-07-19 10:23:55 +0800

    child class -> parent class

--- test17.py
+++ test17.py
@@ -14,7 +14,7 @@ person_list.append(dict3)
 # print person_list
 # for item in person_list:
 # 	print item['name'],item['gender']
-class Person:
+class Person(object):
 	def __init__(self,name,gender):
 		self._name = name  #不是私有的
 		self._gender = gender
@@ -24,6 +24,7 @@ class Person:
 class Sportsman(Person):
 	def __init__(self,name,gender,sports):
 		Person.__init__(self,name,gender)  # 如何调用父类的初始化方法？？？
+		# super(Sportsman,self).__init__(name,gender) #使用super,必须父类是继承于某个类，实在不行可以继承object，并且__init__()的参数不能有self
 		self._sports = sports
 	def get_name_gender_sports(self):
 		return Person.get_name_gender(self)[0],Person.get_name_gender(self)[1], self._sports
